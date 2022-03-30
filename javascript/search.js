927662d4302628398382f0abbc25ed36ee561a42
Laxarela <lelakreka@gmail.com>
2022-03-30 20:10:15 +0200

    menu

---
+++ javascript/search.js
@@ -0,0 +1,34 @@
+document.querySelector("#search-input").addEventListener('input',filterList);
+
+function filterList (){
+    const searchInput = document.querySelector('#search-input');
+    const filter = searchInput.ariaValueMax.toLocaleLowerCase();
+    const listItems = document.querySelectorAll('.food-items');
+    listItems.forEach((item) =>{
+        let text = item.textContent;
+        if(text.toLocaleLowerCase().includes(filter.toLocaleLowerCase())){
+            item.style.display="";
+        }
+        else{
+            item.style.display= 'none';
+        }
+    })
+
+}
+
+// function myFunction() {
+//     var input, filter, ul, li, a, i, txtValue;
+//     input = document.getElementById("search-input");
+//     filter = input.value.toUpperCase();
+//     ul = document.getElementsByClassName(".food-items");
+//     li = ul.getElementsByClassName(".food-img");
+//     for (i = 0; i < li.length; i++) {
+//         a = li[i].getElementsByClassName(".food-name")[0];
+//         txtValue = a.textContent || a.innerText;
+//         if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//             li[i].style.display = "";
+//         } else {
+//             li[i].style.display = "none";
+//         }
+//     }
+// }
